d2c9be0b7135536fe1e74e5c796de815c4f0fe6c
jhark <jhonmark.villegas.31@gmail.com>
2026-01-17 16:13:54 +0800

    feat: adding authenticable concern

---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,21 @@
+module Authenticable
+  def current_user
+    @current_user ||= find_user_from_token
+  end
+
+  private
+
+  def find_user_from_token
+    header = request.headers["Authorization"]
+    return nil unless header
+
+    token = header.split(" ").last
+
+    begin
+      decoded = JsonWebToken.decode(token)
+      User.find_by(id: decoded&.dig("user_id"))
+    rescue JWT::DecodeError
+      nil
+    end
+  end
+end
---
+++ test/controllers/concerns/authenticable_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class MockController < ApplicationController
+  include Authenticable
+  attr_accessor :request
+
+  def initialize
+    mock_request = Struct.new(:headers).new({})
+    self.request = mock_request
+  end
+end
+
+class AuthenticableTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    @authentication = MockController.new
+  end
+
+  test "should get current user" do
+    @authentication.request.headers["Authorization"] = "Bearer #{JsonWebToken.encode({ user_id: @user.id })}"
+
+    assert_not_nil @authentication.current_user
+    assert_equal @user.id, @authentication.current_user.id
+  end
+
+  test "should not get user from empty Authorization token" do
+    @authentication.request.headers["Authorization"] = nil
+    assert_nil @authentication.current_user
+  end
+end
