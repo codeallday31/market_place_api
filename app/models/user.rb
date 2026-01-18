9354eb8343688f1295a2f64c786e84ec26202ce9
jhark <jhonmark.villegas.31@gmail.com>
2026-01-18 15:09:09 +0800

    feat: create test for product and add association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: /@/ }
   validates :password_digest, presence: true
   has_secure_password
+
+  has_many :products, dependent: :destroy
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have a positive price" do
+    product = products(:one)
+    product.price = -1
+    assert_not product.valid?
+  end
+
+  test "destroy user should destroy linked product" do
+    assert_difference("Product.count", -1) do
+      users(:one).destroy
+    end
+  end
 end
