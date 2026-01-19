e0211cf20804f20630ebc73c8bc770209206cd89
jhark <jhonmark.villegas.31@gmail.com>
2026-01-19 12:36:06 +0800

    feat: create order index action

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
-
       post "tokens/create"
-
       resources :products
+      resources :orders, only: %i[index]
     end
   end
 end
