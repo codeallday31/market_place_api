3d1b939ea16d7b8235e9945af40d3f84b7b6803e
jhark <jhonmark.villegas.31@gmail.com>
2026-01-22 13:44:22 +0800

    feat: order pagination

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -9,10 +9,10 @@ class Api::V1::ProductsController < ApplicationController
     products = Product.page(params[:page]).per(params[:per_page]).search(params)
     options = {
       links: {
-        first: api_v1_products_url(page: 1),
-        last: api_v1_products_url(page: products.total_pages),
-        prev: api_v1_products_url(page: products.prev_page),
-        next: api_v1_products_url(page: products.next_page)
+        first: api_v1_products_path(page: 1),
+        last: api_v1_products_path(page: products.total_pages),
+        prev: api_v1_products_path(page: products.prev_page),
+        next: api_v1_products_path(page: products.next_page)
       }
     }
     render json: ProductSerializer.new(products, options).serializable_hash.to_json
